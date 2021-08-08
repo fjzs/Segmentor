d1237d67afe6a1ea88d063502e2a5a9754b50a8a
Adi Nugraha <adinrh@gmail.com>
2021-08-08 13:47:17 -0700

    Remove validation2 folder

--- main_iou.py
+++ main_iou.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 from img_utils import *
-#from stat_perform import *
-from utilities import *
+from stat_perform import *
+#from utilities import *
 
 '''
 This program will calculate the IoU per emage per class
@@ -24,11 +24,13 @@ mdl      = 'modelDeepLabV3_Mila.tflite'
 csv_in   = 'pascal_segmented_classes_per_image.csv'
 csv_out  = 'IOU_perclass.csv'
 
+
 labels = pd.read_csv(tst_path + csv_in, index_col=1).drop('Unnamed: 0', axis = 1)
 label_array = labels.to_numpy()
 col_head = labels.columns
-val_path = tst_path + 'Segmentation_input/validation2/' 
-val_path = 'C:/Users/adi/Downloads/VOC2012/SegmentationClass/'
+val_path = tst_path + 'Segmentation_input/validation/'
+seg_path = tst_path + 'Segmentation_output/validation/' 
+#val_path = 'C:/Users/adi/Downloads/VOC2012/SegmentationClass/'
 image_list = os.listdir(val_path)
 
 # # Get the test image
@@ -42,8 +44,9 @@ nimg = len(image_list)
 # Loop to compute the IOU
 for img in image_list:
         print('Processing image:' + img + ', immage no ' + str(i) + ' of ' + str(nimg))
-        anno_mat =  np.array(Image.open(val_path + img))
-        iou_score, ipclass, time_milisecs = iou_per_class(mdl_path + mdl, val_path + img, labels)
+        anno_mat =  np.array(Image.open(seg_path + os.path.splitext(img)[0] + '.png'))
+        #iou_score, ipclass, time_milisecs = iou_per_class(mdl_path + mdl, val_path + img, labels)
+        iou_score, time_milisecs = imgs_iou(mdl_path + mdl, val_path + img, anno_mat)
         iou_out.append(np.hstack((iou_score, np.squeeze(ipclass), time_milisecs)))
         i=i+1
 iou_out = np.array(iou_out)
--- stat_perform.py
+++ stat_perform.py
@@ -116,7 +116,7 @@ def iou_m(model, labels, image_for_prediction): #can change the model
 
 def iou_per_class(model, image_for_prediction, labels): #can change the model as needed
   '''
-  call function like: utilities.iou_per_class('modelDeepLabV3_Mila.tflite', '2008_000491.jpg')
+  call function like: utilities.iou_per_class('modelDeepLabV3_Mila.tflite', '2008_000491.jpg', labels)
     What does this function do?
     - This function will calculate the IoU
     The input image will be transformed to a segmentation map where the classes per pixel value will be found.
@@ -125,6 +125,9 @@ def iou_per_class(model, image_for_prediction, labels): #can change the model as
     Arguments:
     - param1 (.tflite): a  segmentation model
     - param2 (.jpg): a picture from pascal
+    - labels (pd.DataFrame) : A dataFrame containing the labels to be compared , first column must include the images and the name should be image_filename	
+    the rest of the columns should be labeled 0 to 19 and correspond to the class number of the data set
+    see:  pd.read_csv('https://raw.githubusercontent.com/fjzs/Segmentor/main/datasets/pascal/pascal_segmented_classes_per_image.csv', index_col=1).drop('Unnamed: 0', axis = 1)
 
     Returns:
     - iou_score (float), iou_per_class_array (float array size 1x20 an entry per class), time_milisecs (time in miliseconds)
@@ -195,24 +198,88 @@ def iou_per_class(model, image_for_prediction, labels): #can change the model as
   #real iou part 
   prediction = np.unique(seg_map)
   #For the target, find the label array corresponding to the input image
-  #change pascal_segmented_classes_per_image.csv to local path
-  #labels = pd.read_csv('https://raw.githubusercontent.com/fjzs/Segmentor/main/datasets/pascal/pascal_segmented_classes_per_image.csv', index_col=1).drop('Unnamed: 0', axis = 1)
   specific_pic_classes = labels.filter(like= os.path.basename(image_name),  axis =0)#here test
   specific_pic_class_array = specific_pic_classes.to_numpy()
   
 
   vector_form = np.zeros((20,1))
+  prediction=prediction[prediction != 0]-1
   vector_form [prediction] = 1
   vector_form = vector_form.T
   
 
   target = specific_pic_class_array.astype('float64')
-  intersection = np.logical_and(target , vector_form)
+  intersection = np.logical_and(target, vector_form)
   union = np.logical_or(target, vector_form)
   iou_score = np.sum(intersection) / np.sum(union)
   iou_per_class_array = np.nan_to_num(intersection/union)
 
-  time_milisecs= np.round((end-start) * 1000,4)
+  time_milisecs= round((end-start) * 1000,4)
   
 
   return iou_score, iou_per_class_array, time_milisecs
+
+def imgs_iou(model, image_for_prediction, val_img): #can change the model as needed
+
+  image_name = image_for_prediction
+  interpreter = tf.lite.Interpreter(model_path=model)
+  # Interpreter interface for TensorFlow Lite Models.
+
+
+  # Gets model input and output details.
+  input_index = interpreter.get_input_details()[0]["index"]
+  input_details = interpreter.get_input_details()
+  output_index = interpreter.get_output_details()[0]["index"]
+  interpreter.allocate_tensors()
+  input_img = mpimg.imread(image_for_prediction)
+  image = Image.fromarray(input_img)
+
+
+  # Get image size - converting from BHWC to WH
+  input_size = input_details[0]['shape'][2], input_details[0]['shape'][1]
+
+  old_size = image.size  # old_size is in (width, height) format
+  desired_ratio = input_size[0] / input_size[1]
+  old_ratio = old_size[0] / old_size[1]
+
+  if old_ratio < desired_ratio: # '<': cropping, '>': padding
+      new_size = (old_size[0], int(old_size[0] / desired_ratio))
+  else:
+      new_size = (int(old_size[1] * desired_ratio), old_size[1])
+ 
+  # Cropping the original image to the desired aspect ratio
+  delta_w = new_size[0] - old_size[0]
+  delta_h = new_size[1] - old_size[1]
+  padding = (delta_w//2, delta_h//2, delta_w-(delta_w//2), delta_h-(delta_h//2))
+  cropped_image = ImageOps.expand(image, padding)
+
+  # Resize the cropped image to the desired model size
+  resized_image = cropped_image.convert('RGB').resize(input_size, Image.BILINEAR)
+
+  # Convert to a NumPy array, add a batch dimension, and normalize the image.
+  image_for_prediction = np.asarray(resized_image).astype(np.float32)
+  image_for_prediction = np.expand_dims(image_for_prediction, 0)
+  image_for_prediction = image_for_prediction / 127.5 - 1
+    
+  # Invoke the interpreter to run inference.
+  
+  interpreter.set_tensor(input_details[0]['index'], image_for_prediction)
+  interpreter.invoke()
+
+  #get values of input sizes **********
+  input_size = input_details[0]['shape'][2], input_details[0]['shape'][1]
+
+  # Sets the value of the input tensor
+  interpreter.set_tensor(input_details[0]['index'], image_for_prediction)
+   # Invoke the interpreter.
+  interpreter.invoke()
+  start = time.time()
+  predictions_array = interpreter.get_tensor(output_index)
+  end = time.time()
+  mrst = tf.keras.metrics.MeanIoU(num_classes=21)
+  mrst.update_state(val_img, predictions_array)
+  iou_score = mrst.result().numpy()
+  time_milisecs= round((end-start) * 1000,4)
+  
+
+  return iou_score, time_milisecs
