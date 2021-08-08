a8e5778db70f21f0c11822fdd567ad5106ee76e4
Ruzick <57594261+Ruzick@users.noreply.github.com>
2021-08-08 16:50:18 -0400

    Update inference.py

--- inference.py
+++ inference.py
@@ -25,7 +25,7 @@ def get_category(img, model):
         [str]: Prediction
     """
         #Prepare image further for running inference *******
-    path = 'static/model/deep_lab_v3_plus'
+    path = 'static/models/deep_lab_v3_plus'
     tflite_model_file = model
     # Load TFLite model and allocate tensors.
     with open(path + tflite_model_file, 'rb') as fid:
